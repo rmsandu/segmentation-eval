cef404281365603091a630e365ba8e8611d18606
raluca-san <ralucam.sandu@gmail.com>
2018-05-07 20:03:01 +0200

    paste roi mask in the same dimensions as source DICOM

--- paste_roi_image.py
+++ paste_roi_image.py
@@ -8,55 +8,26 @@ Created on Thu Apr 26 16:00:45 2018
 import SimpleITK as sitk
 
 
-def paste_roi_image(image_plan, image_validation, image_roi):
-    ''' Usage: newImage = resize_image(source_img_plan, source_img_validation, roi_tumor_mask) '''
+def paste_roi_image(image_source, image_roi):
+    ''' Usage: newImage = resize_image(source_img_plan, roi_mask) '''
         
-    sizeP = image_plan.GetSize()
-    sizeV = image_validation.GetSize()
-    if sizeP[2] > sizeV[2]:
-        newSize = (sizeP[0], sizeP[1], sizeP[2])
-    else:
-        newSize = (sizeP[0], sizeP[1], sizeV[2])
-     
-    if sizeP[1]  > sizeV[1]:
-        newSize = (sizeP[0], sizeP[1], newSize[2])
-    else:
-        newSize = (sizeP[0], sizeV[1], newSize[2])
-    
-    if sizeP[0] > sizeV[0]:
-        newSize = (sizeP[0], newSize[1], newSize[2])
-    else:
-        newSize = (sizeV[0], newSize[1], newSize[2])
-        
-        
-        
-    originV = image_validation.GetOrigin()   
-    originP = image_plan.GetOrigin()
-    newOrigin = ()
-    for idx, val in enumerate(originP):
-        if  abs(originP[idx]) > abs(originV[idx]):
-            newOrigin = newOrigin + (originP[idx],)
-        else:
-            newOrigin = newOrigin + (originV[idx],)
-     
+    newSize = image_source.GetSize()
+    newOrigin = image_source.GetOrigin()   
     # we assume we have the same spacing as the images have been taken with the same scanner    
-    spacingP = image_plan.GetSpacing() 
-#    newOriginResampled = (newOrigin[0]*newSize[0]*spacingP[0], newOrigin[1]*newSize[1]*spacingP[1],\
-#                        newOrigin[2]*newSize[2]*spacingP[2])
-    directionP = image_plan.GetDirection()
+    newSpacing = image_source.GetSpacing() 
+    newDirection = image_source.GetDirection()
     
     # re-cast the pixel type of the roi mask
-    pixelID = image_plan.GetPixelID()
+    pixelID = image_source.GetPixelID()
     caster = sitk.CastImageFilter()
     caster.SetOutputPixelType( pixelID )
     image_roi = caster.Execute(image_roi)
  
     # black 3D image
     outputImage = sitk.Image(newSize, sitk.sitkInt16)
-#    outputImage.SetPixelAsComplexFloat64() 
     outputImage.SetOrigin(newOrigin)
-    outputImage.SetSpacing(spacingP)
-    outputImage.SetDirection(directionP)    
+    outputImage.SetSpacing(newSpacing)
+    outputImage.SetDirection(newDirection)    
     # transform from physical point to index the origin of the ROI image
     destinationIndex = outputImage.TransformPhysicalPointToIndex(image_roi.GetOrigin())
     pasted_img = sitk.Paste(outputImage, image_roi, image_roi.GetSize(), destinationIndex=destinationIndex)
