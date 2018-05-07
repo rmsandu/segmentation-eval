e7464f8d6a45d0276bc1f3f8caff8bcb9d27262a
raluca-san <ralucam.sandu@gmail.com>
2018-05-07 20:01:41 +0200

    SimpleItk Overlay Filter to display overlaid segmentation mask on DICOM

--- mainPreProcessingSegmentationMasks.py
+++ mainPreProcessingSegmentationMasks.py
@@ -6,12 +6,14 @@ Created on Thu May  3 14:25:13 2018
 """
 
 
+
 import SimpleITK as sitk
-import pydicom as dicom
-from paste_roi_image import paste_roi_image
 import read_dcm_series as reader
 import write_dcm_series_v2 as writeDCM
+from paste_roi_image import paste_roi_image
+from sitkShowSlice import sitk_show
 
+#%%
 folder_path_tumor = "C:/develop/data/Segmentation_tumor"
 tumor_mask = reader.read_dcm_series(folder_path_tumor)
 folder_path_ablation = "C:/develop/data/Segmentation_ablation"
@@ -29,12 +31,28 @@ reader.print_dimensions_img('validation', source_img_validation)
 
 
 #%%
-resizedTumorMask = paste_roi_image(source_img_plan, source_img_validation, tumor_mask)
-writeDCM.writeDICOMSeries("C:/develop/data/Source_CT_Plan","C:/develop/data/Resized_Segmentation_Tumor/", resizedTumorMask)
-resizedAblationMask = paste_roi_image(source_img_plan, source_img_validation, ablation_mask)
-writeDCM.writeDICOMSeries("C:/develop/data/Source_CT_Plan","C:/develop/data/Resized_Segmentation_Ablation/", resizedAblationMask)
-# TO DO: adapt the number of slices
+# resize the Segmentation Mask to the dimensions of the source images they were derived from
+resizedTumorMask = paste_roi_image(source_img_plan, tumor_mask)
+resizedAblationMask = paste_roi_image( source_img_validation, ablation_mask)
+
+#writeDCM.writeDICOMSeries("C:/develop/data/Source_CT_Plan","C:/develop/data/Resized_Segmentation_Tumor/", resizedTumorMask)
+#writeDCM.writeDICOMSeries("C:/develop/data/Source_CT_Plan","C:/develop/data/Resized_Segmentation_Ablation/", resizedAblationMask)
+# TO DO:
+    # - adapt the number of slices for the ablation mask
+    # - it seems that the mask for the tumor is not copied at the right location . but why?
+    # - the ablation mask is copied on different slices. make it the same number of slices as the source img it was derived from (CT validation)
+    # - check if you can run eval segmentation metrics if the tumor and ablation mask have different sizes (nr. of slices)
+    #
 #sitk.Show(resizedAblationMask)
 #sitk.Show(resizedTumorMask)
 #%%
-new_folder = "C:\develop\data\Resized_Segmentation"
+idxSlice = 122
+labelTumor = 1
+imgOriginal = source_img_plan[:,:,idxSlice]
+imgMask = resizedTumorMask[:,:,idxSlice]
+
+img_T1_255 = sitk.Cast(sitk.RescaleIntensity(source_img_plan), sitk.sitkUInt8)
+seg = sitk.Cast(sitk.RescaleIntensity(resizedTumorMask), sitk.sitkUInt8)
+OverlaidImg = sitk.LabelOverlay(img_T1_255, seg, opacity=0.2)
+
+sitk_show(OverlaidImg[:,:,idxSlice])
