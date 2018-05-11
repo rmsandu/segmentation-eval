2d02ccc192e3bbd70f7ef044d3615741d9a75555
raluca-san <ralucam.sandu@gmail.com>
2018-05-11 13:46:51 +0200

    functioning animation, disaply tumor label over CT slices

--- animationDICOM.py
+++ animationDICOM.py
@@ -6,22 +6,36 @@ Created on Tue May  8 13:36:39 2018
 """
 
 import numpy as np
-import numpy.ma as ma
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 #def AnimateDICOM(SourceImg, MaskImg):
+#global SourceImg
 slices, x , y = SourceImg.shape
 fig = plt.figure()
 
 im = plt.imshow(SourceImg[0,:,:], cmap=plt.cm.gray, interpolation='none', animated=True)
+im2 = plt.imshow(mask_tumor_nda[0,:,:], cmap='RdYlBu', alpha=0.3, interpolation='none',animated=True)
+ims = []
 
 def updatefig(z):
     global SourceImg
+    global mask_tumor_nda
+    
+    TumorOverlay = mask_tumor_nda[z,:,:].astype(np.float)
+    TumorOverlay[TumorOverlay ==0 ] = np.nan
+
     new_slice = SourceImg[z,:,:]
+    
+    im2.set_array(TumorOverlay)
     im.set_array(new_slice)
-    return im
+    ims.append([im])
+    ims.append([im2])
+    return [ims]
 
-ani = animation.FuncAnimation(fig, updatefig, np.arange(1, slices), interval=20, blit=True)
+ani = animation.FuncAnimation(fig, updatefig, frames = np.arange(1, slices), interval=25)
+# blit =  True option to re-draw only the parts that have changed
+# repeat_delay=1000
 plt.show() 
-
+#ani.save('animationTumor.gif', writer='imagemagick', fps=10)
+#ani.save('animation.mp4', writer='ffmpeg' ,fps=10)
