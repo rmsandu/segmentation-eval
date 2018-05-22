c8b8350dcdca1bd1149023aa6ff4285a36077afe
raluca-san <ralucam.sandu@gmail.com>
2018-05-22 13:41:24 +0200

    create WriteToDICOM Class, read multiple csv to dictionary list

--- createTumorAblationsFilePaths.py
+++ createTumorAblationsFilePaths.py
@@ -5,37 +5,50 @@ Created on Mon May 14 11:01:48 2018
 @author: Raluca Sandu
 """
 import os
+import csv
 import pandas as pd
-import mainDistanceVolumeMetrics
 
 #%%
 # rootdir = "Z:/Public/Raluca_Radek/GroundTruth_2017/"
 
-rootdir = r"C:\develop\data"
-counter = 0
+'''
+1. give the cochlea folder path as input where the masks are
+2. iterate through file and folders
+3. look for csv file open it and append the information to dictionary (?!)/dataframe/CSV (?!)
+4. open the csv and convert it to dataframe
+5. read the file paths - call the function reader
+6. call the function resizePaste
+7. call the function write to DICOM Series
+ - create new folder for each patient where to write (write on server? write local repo?)
+ - set name of folder accordingly
+
+'''
+#%%
+# rootdir = r"C:\develop\data"
+rootdir = r"Z:\Public\Raluca_Radek\GroundTruth_2018\GT_23042018"
+patientID = 0
 # list of dictionaries containing the filepaths of the segmentations
-FilepathsDictionary = [] 
+result = {}
+
 for path, dirs, files in os.walk(rootdir):
     tumorFilePath = ''
     ablationFilePath = ''
-    for subdir in dirs:
-        if "Ablation" in subdir:
-            ablationFilePath = os.path.join(path, subdir)
-            ablationFilePath = os.path.normpath(ablationFilePath)
-        if "Tumor" in subdir:
-            tumorFilePath = os.path.join(path, subdir)
-            tumorFilePath = os.path.normpath(tumorFilePath)
-#            
-    if tumorFilePath and ablationFilePath:
-        counter += 1
 
-        data = {'PatientName' : counter,
-                'TumorFile' : tumorFilePath,
-                'AblationFile' : ablationFilePath
-                }
-        FilepathsDictionary.append(data)  
+    for file in files:
+        fileName, fileExtension = os.path.splitext(file)
+        if fileExtension.endswith('.csv') and 'filepaths' in fileName.upper().lower():
+            patientID +=1
+            filepath_csv = os.path.normpath(os.path.join(path, file))
+            reader = csv.DictReader(open(filepath_csv))
+            for row in reader:
+                for column, value in row.items():
+                    if column != 'TrajectoryID':
+                        file_value = path + value
+                    else:
+                        file_value = value
+                    result.setdefault(column, []).append(file_value)
+                # add patient ID column
+                result.setdefault('PatientID', []).append(patientID)
+                    
+df_filepaths = pd.DataFrame(result)
 
-# convert the filepaths to data frame 
-df_patientdata = pd.DataFrame(FilepathsDictionary)
-# call the function for parsing distances
-mainDistanceVolumeMetrics.main_distance_metrics(df_patientdata, rootdir)
--- writeToDICOMSeries.py
+++ writeToDICOMSeries.py
@@ -3,54 +3,108 @@
 Created on Thu May  3 15:40:46 2018
 
 @author: Raluca Sandu
-"""
-import time, os
-import SimpleITK as sitk
-
 # Read the original series. First obtain the series file names using the
 # image series reader. Then read each image using an image reader that is
 # set to load all DICOM tags (public+private). The resulting images contain
 # their DICOM meta-data dictionaries.
-def writeDICOMSeries(folderOutput, img_pasted):
+"""
+import os
+import time
+import uuid
+import hashlib
+from random import random
+import SimpleITK as sitk
 
-    # Use the study/seriers/frame of reference information given in the meta-data
-    # dictionary and not the automatically generated information from the file IO
-    writer = sitk.ImageFileWriter()
-    writer.KeepOriginalImageUIDOn()
-    modification_time = time.strftime("%H%M%S")
-    modification_date = time.strftime("%Y%m%d")
-    direction = img_pasted.GetDirection()
-#    spacing = img_pasted.GetSpacing()
-    
 
-    series_tag_values = [("0008|0031", modification_time), # Series Time
-                  ("0008|0021", modification_date), # Series Date
-                  ("0008|0008","DERIVED\\SECONDARY"), # Image Type
-                  ("0020|000e", "1.2.826.0.1.3680043.2.1125."+modification_date+".1"+modification_time), # Series Instance UID
-                  ("0020|0037", '\\'.join(map(str, (direction[0], direction[3], direction[6],# Image Orientation (Patient)
-                                                    direction[1],direction[4],direction[7])))),
-                  ("0008|103e", "ResizedSegmentationMask")] # Series Description
-    
-    for i in range(img_pasted.GetDepth()):
-        image_slice = img_pasted[:,:,i]
-        # Tags shared by the series
-        for tag, value in series_tag_values:
-            # Slice specific tags
-            image_slice.SetMetaData(tag,value)
-        image_slice.SetMetaData("0008|0012", time.strftime("%Y%m%d")) # Instance Creation Date
-        image_slice.SetMetaData("0008|0013", time.strftime("%H%M%S")) # Instance Creation Time
-        # Setting the type to CT preserves the slice location.
-        image_slice.SetMetaData("0008|0060", "CT")  # set the type to CT so the thickness is carried over
-        # (0020, 0032) image position patient determines the 3D spacing between slices.
-        image_slice.SetMetaData("0020|0032", '\\'.join(map(str, img_pasted.TransformIndexToPhysicalPoint((0,0,i))))) # Image Position (Patient)
-        image_slice.SetMetaData("0020,0013", str(i)) # Instance Number
-        image_slice.SetMetaData("0008|0008", "DERIVED\SECONDARY")
-        
-        # Copy the meta-data except the PixelSpacing, PatientPosition, SliceThickness
-#        if k!="0028|0030" and k!= "0018|5100" and k!="0018|0050" :
-#        for k in original_slice.GetMetaDataKeys():
-#            image_slice.SetMetaData(k, original_slice.GetMetaData(k))
-#            
-         # Write to the output directory and add the extension dcm, to force writing in DICOM format.   
-        writer.SetFileName(os.path.normpath(folderOutput + '/' + 'ablationSegm' + str(i)  + '.dcm'))
-        writer.Execute(image_slice)
+def make_uid(entropy_srcs=None, prefix='2.25.'):
+    """Generate a DICOM UID value.
+    Follows the advice given at:
+    http://www.dclunie.com/medical-image-faq/html/part2.html#UID
+    Parameters
+    ----------
+    entropy_srcs : list of str or None
+        List of strings providing the entropy used to generate the UID. If
+        None these will be collected from a combination of HW address, time,
+        process ID, and randomness.
+    prefix : prefix
+    """
+    # Combine all the entropy sources with a hashing algorithm
+    if entropy_srcs is None:
+        entropy_srcs = [str(uuid.uuid1()),  # 128-bit from MAC/time/randomness
+                        str(os.getpid()),  # Current process ID
+                        random().hex()  # 64-bit randomness
+                       ]
+    hash_val = hashlib.sha256(''.join(entropy_srcs))
+    # Convert this to an int with the maximum available digits
+    avail_digits = 64 - len(prefix)
+    int_val = int(hash_val.hexdigest(), 16) % (10 ** avail_digits)
+
+    return prefix + str(int_val)
+
+
+class DicomWriter:
+
+    def __init__(self, img_pasted, img_source, folder_output, file_name, patient_id):
+        """
+        :type img_pasted: re-sized binary segmentation image in SimpleITK format
+        :type img_source: source image in SimpleITK format
+        :type folder_output: folder path to write the DICOM Series Files
+        :type file_name: string specifying the filename, ablation or tumor z.B.
+        :type patient_id: string number denoting unique patient ID
+        """
+        self.img_pasted = img_pasted
+        self.img_source = img_source
+        self.folder_output = folder_output
+        self.patient_id = patient_id
+        self.series_instance_uid = make_uid()
+        self.study_instance_uid = make_uid()
+        self.file_name = file_name
+
+    def save_image_to_file(self):
+        # Use the study/seriers/frame of reference information given in the meta-data
+        # dictionary and not the automatically generated information from the file IO
+        writer = sitk.ImageFileWriter()
+        writer.KeepOriginalImageUIDOn()
+        modification_time = time.strftime("%H%M%S")
+        modification_date = time.strftime("%Y%m%d")
+        direction = self.img_pasted.GetDirection()
+        spacing = self.img_pasted.GetSpacing()
+
+        series_tag_values = [("0010|0020", self.patient_id),  # set patientID
+                             ("0008|0031", modification_time),  # Series Time
+                             ("0008|0021", modification_date),  # Series Date
+                             ("0008,0016", '1.2.840.10008.5.1.4.1.1.66.4'),  # SOP Segmentation Class UID
+                             ("0020|000e", self.series_instance_uid + modification_time),  # Series Instance ID
+                             ("0020|000D", self.study_instance_uid + modification_time),  # Study Instance ID
+                             ("0008|0008", "DERIVED\\SECONDARY"),  # Image Type
+                             # Series Instance UID
+                             ("0020|000e", "1.2.826.0.1.3680043.2.1125." + modification_date + ".1" + modification_time),
+                             # Image Orientation (Patient)
+                             ("0020|0037", '\\'.join(map(str, (direction[0], direction[3], direction[6],
+                                                               direction[1], direction[4], direction[7])))),
+                             # Pixel Spacing
+                             ("0028|0030", '\\'.join(map(str, (spacing[0], spacing[1], spacing[2])))),
+                             ("0008|103e", "BINARY SEGMENTATION MASK")]  # Series Description
+
+        # set patient sex ,Bits Stored, Bits Allocated, Samples per Pixel
+        # set SliceThickness "0018|0050", set PatientPosition  "0018|5100"
+        # set Size
+
+        for i in range(self.img_pasted.GetDepth()):
+            image_slice = self.img_pasted[:, :, i]
+            # Tags shared by the series
+            for tag, value in series_tag_values:
+                # Slice specific tags
+                image_slice.SetMetaData(tag, value)
+            image_slice.SetMetaData("0008|0012", time.strftime("%Y%m%d"))  # Instance Creation Date
+            image_slice.SetMetaData("0008|0013", time.strftime("%H%M%S"))  # Instance Creation Time
+            # Setting the type to CT preserves the slice location.
+            image_slice.SetMetaData("0008|0060", "CT")  # set the type to CT so the thickness is carried over
+            # (0020, 0032) image position patient determines the 3D spacing between slices.
+            # Image Position (Patient)
+            image_slice.SetMetaData("0020|0032", '\\'.join(map(str, self.img_pasted.TransformIndexToPhysicalPoint((0, 0, i)))))
+            image_slice.SetMetaData("0020,0013", str(i))  # Instance Number
+
+            # Write to the output directory and add the extension dcm, to force writing in DICOM format.
+            writer.SetFileName(os.path.normpath(self.folder_output + '/' + self.file_name + str(i) + '.dcm'))
+            writer.Execute(image_slice)
