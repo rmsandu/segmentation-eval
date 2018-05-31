adae3790490db9880efab7128915479007fcea5d
raluca-san <ralucam.sandu@gmail.com>
2018-05-31 11:45:54 +0200

    output Excel with new filepaths of segmentations

--- ResizeSegmentations.py
+++ ResizeSegmentations.py
@@ -6,6 +6,7 @@ Created on Wed May 23 14:42:27 2018
 """
 
 import os
+import pandas as pd
 import DicomReader as Reader
 import DicomWriter as DicomWriter
 import PasteRoiImage as PasteRoi
@@ -14,34 +15,33 @@ import PasteRoiImage as PasteRoi
 class ResizeSegmentations:
 
     def __init__(self, df_folderpaths):
-        self.ablation_paths = df_folderpaths[' Ablation Segmentation Path'].tolist()
-        self.tumor_paths = df_folderpaths[' Tumour Segmentation Path'].tolist()
-        self.folder_path_plan = df_folderpaths['Plan Images Path'].tolist()
-        self.folder_path_validation = df_folderpaths['Validation Images Path'].tolist()
-        self.trajectoryID = df_folderpaths['TrajectoryID'].tolist()
-        self.patients = df_folderpaths['PatientID'].tolist()
-        # tolist() might be unnecessary
+        self.df_folderpaths = df_folderpaths
+        self.new_filepaths = []
 
     def save_images_to_disk(self, root_path_to_save):
+        ablation_paths = self.df_folderpaths[' Ablation Segmentation Path'].tolist()
+        tumor_paths = self.df_folderpaths[' Tumour Segmentation Path'].tolist()
+        folder_path_plan = self.df_folderpaths['Plan Images Path'].tolist()
+        folder_path_validation = self.df_folderpaths['Validation Images Path'].tolist()
+        trajectoryID = self.df_folderpaths['TrajectoryID'].tolist()
+        patients = self.df_folderpaths['PatientID'].tolist()
 
-        for idx, ablation_path in enumerate(self.ablation_paths):
+        for idx, ablation_path in enumerate(ablation_paths):
 
-            print(str(self.patients[idx]))
-            tumor_mask = Reader.read_dcm_series(self.tumor_paths[idx])
-     
+            tumor_mask = Reader.read_dcm_series(tumor_paths[idx])
             ablation_mask = Reader.read_dcm_series(ablation_path)
-            source_img_plan = Reader.read_dcm_series(self.folder_path_plan[idx])
-            source_img_validation = Reader.read_dcm_series(self.folder_path_validation[idx])
+            source_img_plan = Reader.read_dcm_series(folder_path_plan[idx])
+            source_img_validation = Reader.read_dcm_series(folder_path_validation[idx])
+
             # resize the Segmentation Mask to the dimensions of the source images they were derived from '''
             resized_tumor_mask = PasteRoi.paste_roi_imageMaxSize(source_img_plan, source_img_validation,
                                                                  tumor_mask)
             resized_ablation_mask = PasteRoi.paste_roi_imageMaxSize(source_img_plan, source_img_validation,
                                                                     ablation_mask)
-
+            # create folder directories to write the new segmentations
             parent_directory = os.path.join(root_path_to_save,
-                                            'Pat_GTDB_' + str(self.patients[idx]))
-
-            child_directory_trajectory = os.path.join(parent_directory, 'Trajectory' + str(self.trajectoryID[idx]))
+                                            'Pat_GTDB_' + str(patients[idx]))
+            child_directory_trajectory = os.path.join(parent_directory, 'Trajectory' + str(trajectoryID[idx]))
             child_directory_tumor = os.path.join(parent_directory,
                                                  child_directory_trajectory,
                                                  'Resized_Tumor_Segmentation')
@@ -62,15 +62,25 @@ class ResizeSegmentations:
                     # trajectory folder exists, re-write segmentations
                     os.makedirs(child_directory_tumor)
                     os.makedirs(child_directory_ablation)
+            # save new filepaths to dictionary
+            dict_paths = {
+                    ' Tumour Segmentation Path Resized':  child_directory_tumor,
+                    ' Ablation Segmentation Path Resized': child_directory_ablation
+                }
+            self.new_filepaths.append(dict_paths)
 
             # Save the Re-sized Segmentations to DICOM Series
             obj_writer1 = DicomWriter.DicomWriter(resized_tumor_mask, source_img_plan,
                                                   child_directory_tumor,
-                                                  'tumorSegm', str(self.patients[idx]))
+                                                  'tumorSegm', str(patients[idx]))
             obj_writer1.save_image_to_file()
 
             obj_writer2 = DicomWriter.DicomWriter(resized_ablation_mask, source_img_validation,
                                                   child_directory_ablation,
-                                                  'ablationSegm', str(self.patients[idx]))
+                                                  'ablationSegm', str(patients[idx]))
             obj_writer2.save_image_to_file()
 
+    def get_new_filepaths(self):
+        df_new_filepaths = pd.DataFrame(self.new_filepaths)
+        df_final = pd.concat([self.df_folderpaths, df_new_filepaths], axis=1)
+        return df_final
--- create_filepaths_resized_segmentations.py
+++ create_filepaths_resized_segmentations.py
@@ -1,10 +1,12 @@
 import os
+import time
 import csv
 import pandas as pd
 import ResizeSegmentations as ReaderWriterClass
 
 rootdir = r"C:\PatientDatasets_GroundTruth_Database\GroundTruth_2018\GT_23042018"
-patientID = 0
+# start patient counting at 10, not to confuse with previous dataset 2017 where patients are name 1-10
+patientID = 10
 # list of dictionaries containing the filepaths of the segmentations
 dictionary_filepaths = {}
 
@@ -29,6 +31,16 @@ for path, dirs, files in os.walk(rootdir):
                 dictionary_filepaths.setdefault('PatientID', []).append(patientID)
 
 df_filepaths = pd.DataFrame(dictionary_filepaths)
+# Call The ReaderWriterClass to Resize Segmentations
 resize_object = ReaderWriterClass.ResizeSegmentations(df_filepaths)
 folder_path_saving = r"C:\PatientDatasets_GroundTruth_Database\GroundTruth_2018\GT_23042018_anonymized"
 resize_object.save_images_to_disk(folder_path_saving)
+df_new_filepaths = resize_object.get_new_filepaths()
+
+#%%
+timestr = time.strftime("%H%M%S-%Y%m%d")
+filename = 'FilepathsResizedGTSegmentations' + '_' + timestr + '.xlsx'
+filepathExcel = os.path.join(rootdir, filename)
+writer = pd.ExcelWriter(filepathExcel)
+df_new_filepaths.to_excel(writer, index=False)
+print("success")
