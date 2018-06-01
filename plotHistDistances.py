plot volumes. correct volume spacing calculation

plotHistDistances.py
@@ -38,7 +38,7 @@ def plotHistDistances(pat_name, pat_idx, rootdir, distanceMap, num_voxels, title
         elif b >= 5:
             voxels_ablated.append(col_val)
 #%%
-    '''calculate the total percentage of surface for ablated, non-ablated, insufficently ablated'''
+    '''calculate the total percentage of surface for ablated, non-ablated, insufficiently ablated'''
     voxels_nonablated = np.asarray(voxels_nonablated)
     voxels_insuffablated = np.asarray(voxels_insuffablated)
     voxels_ablated = np.asarray(voxels_ablated)
@@ -49,9 +49,9 @@ def plotHistDistances(pat_name, pat_idx, rootdir, distanceMap, num_voxels, title
 #%%
     '''iterate through the bins to change the colors of the patches bases on the range [mm]'''
     for b, p, col_val in zip(bins, patches, col_height):
-        if b < 0:
+        if b <= 0:
             plt.setp(p, 'facecolor', 'darkred', label='Non-ablated Surface: ' + " %.2f" % sum_perc_nonablated + '%')
-        elif b in range(0, 5):
+        elif b in range(1, 5):
             plt.setp(p, 'facecolor', 'orange', label='Insufficient Ablation Margin: ' + "%.2f" % sum_perc_insuffablated + '%')
         elif b >= 5:
             plt.setp(p, 'facecolor', 'darkgreen', label='Sufficient Ablation Margin: ' + " %.2f" % sum_perc_ablated + '%')

scatterPlotVolumes.py
@@ -5,70 +5,60 @@ Created on Fri Jan 19 10:43:21 2018
 
 @author: Raluca Sandu
 """
-
-import numpy as np
+import os
 import pandas as pd
+import numpy as np
 import graphing as gh
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 from collections import OrderedDict
-#import seaborn as sns
 plt.style.use('ggplot')
 #%%
-volumes_df = pd.read_excel('volumetricResults.xlsx')
-x = volumes_df[' Tumour Volume (ml)']
-y = volumes_df[' Tumour residual volume (ml)']
-y2 = volumes_df[' Tumor residual volume to tumor volume  ratio']
+filepath_excel = r"C:\PatientDatasets_GroundTruth_Database\DistanceVolumeMetrics_Pooled_Ablation to Tumor Euclidean Distances.xlsx"
+df_volumes = pd.read_excel(filepath_excel)
+x = df_volumes['Tumour Volume (ml)']
+y = df_volumes['Tumour residual volume (ml)']
+y2 = df_volumes[' Tumour coverage ratio']
 fig, ax = plt.subplots()
-ax.scatter(x, y,s=200)
-# use logarithmic scale
-#ax.set_xscale('log')
+ax.scatter(x, y, s=200)
 plt.xlabel(' Tumour Volume (ml)', fontsize=14, color='black')
 plt.ylabel(' Tumour residual volume (ml)', fontsize=14, color='black')
-plt.tick_params(labelsize=14,color='black')
+plt.tick_params(labelsize=14, color='black')
 ax.tick_params(colors='black', labelsize=14)
 #plt.grid(True)
 
 #%%
 '''tumor vs ablation vol'''
-
 fig1, ax1 = plt.subplots()
-ax1.scatter(x,1- y2,s=200)
-# use logarithmic scale
-#ax1.set_xscale('log')
+ax1.scatter(x, y2, s=200)
 plt.xlabel(' Tumour Volume (ml)', fontsize=14, color='black')
 plt.ylabel(' Tumour coverage ratio', fontsize=14, color='black')
-plt.tick_params(labelsize=14,color='black')
+plt.tick_params(labelsize=14, color='black')
 ax1.tick_params(colors='black', labelsize=14)
-
 #%%
 '''tumor vol vs residual tumor vol - rainbow scatter plot'''
 fig2, ax2 = plt.subplots()
-#cmap = ListedColormap(sns.color_palette())
 colors = iter(cm.tab10(np.linspace(0, 1, len(y))))
-#colors = iter(cmap(np.linspace(0, 2, len(y))))
-labels = volumes_df['Patient number'].tolist()
-labels_1 = ['Case ' + str(x) for x in labels]
+labels = df_volumes['PatientID'].tolist()
+labels_1 = ['Case ' + str(x) for x in range(1, len(labels)+1)]
 
-for xs,ys,lab in zip(x, 1-y2,labels_1):
-    ax2.scatter(xs, ys, s=200, marker='o', color=next(colors),label=lab)
-
-#use logarithmic scale    
-
+for xs, ys, lab in zip(x, y2, labels_1):
+    ax2.scatter(xs, ys, s=200, marker='o', color=next(colors), label=lab)
+# use logarithmic scale because tumor volume coverage ratio is [0-1] volume is in ml
 ax2.set_ylim([0, 1.04])
 ax2.set_xlim([0.09, 40])
-ax2.set_xscale('log')   
+ax2.set_xscale('log')
 plt.xlabel(' Tumour Volume (ml)', fontsize=14, color='black')
 plt.ylabel(' Tumour Volume Coverage Ratio', fontsize=14, color='black')
 plt.title('Tumor Volume Coverage Ratio with respect to Tumor volume. 10 Cases')
-plt.tick_params(labelsize=14,color='black')
+plt.tick_params(labelsize=14, color='black')
 ax2.tick_params(colors='black', labelsize=14)
 
 handles, labels = plt.gca().get_legend_handles_labels()
 by_label = OrderedDict(zip(labels, handles))
-leg = plt.legend(by_label.values(), by_label.keys(), fontsize=12, loc='lower left',frameon=True)
+leg = plt.legend(by_label.values(), by_label.keys(), fontsize=12, loc='lower left', frameon=True)
 leg.get_frame().set_edgecolor('k')
 
 # save figure
-figPath = 'ScatterPlot_volumes.png'
-#gh.save(figPath, width=12, height=10)
+figPath = os.path.join('ScatterPlot_volumes.png')
+gh.save(figPath, width=12, height=10)

volumemetrics.py
@@ -31,8 +31,6 @@ class VolumeMetrics:
 
     def get_volume_ml(self, image):
         x_spacing, y_spacing, z_spacing = image.GetSpacing()
-        z_spacing = x_spacing
-        y_spacing = x_spacing
         image_nda = sitk.GetArrayFromImage(image)
         imageSegm_nda_NonZero = image_nda.nonzero()
         num_voxels = len(list(zip(imageSegm_nda_NonZero[0],
@@ -56,9 +54,9 @@ class VolumeMetrics:
         # perform intersection on the common voxel coordinates between tumor and ablation
         intersection_tumor_ablation = np.array([x for x in tumor_set & ablation_set])
         num_voxels_intersection_non_zero = len(intersection_tumor_ablation)
+
+        # Get the spacing
         x_spacing, y_spacing, z_spacing = self.tumorSegm.GetSpacing()
-        y_spacing = x_spacing
-        z_spacing = x_spacing
         # volume_residual = volume_tumor - volume_intersection ablation and tumor
         volume_intersection = (num_voxels_intersection_non_zero * x_spacing * y_spacing * z_spacing) / 1000
         volume_tumor = self.get_volume_ml(self.tumorSegm)