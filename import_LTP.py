f17f9a2127b8b7543ed20ee1275644ff5ca33630
raluca-san <ralucam.sandu@gmail.com>
2019-10-14 16:21:55 +0200

    import LTP information from RedCap download

--- import_LTP.py
+++ import_LTP.py
@@ -6,7 +6,7 @@
 
 import numpy as np
 import pandas as pd
-file_redcap = r"C:\develop\segmentation-eval\redcap_file_all.xlsx"
+file_redcap = r"C:\develop\segmentation-eval\redcap_file_all_2019-10-14.xlsx"
 file_radiomics = r"C:\develop\segmentation-eval\Radiomics_Radii_Chemo_MAVERRIC.xlsx"
 
 # Number of completely ablated lesions
@@ -34,10 +34,10 @@ for idx, patient_id in enumerate(patient_ids):
     no_of_lesions = df_radiomics.iloc[idx_pat_radiomics[0]].Nr_Lesions_Ablated
     no_lesions_total.append(no_of_lesions)
     for index, row in df_patient_redcap.iterrows():
-        if not np.isnan(row['Number of completely ablated lesions']):
-            nr_completely_ablated = row['Number of completely ablated lesions']
-        if not np.isnan(row['Number of incomplete ablations']):
-            nr_incompletely_ablated = row['Number of incomplete ablations']
+        if not np.isnan(row['Number of completely ablated lesions.1']):
+            nr_completely_ablated = row['Number of completely ablated lesions.1']
+        if not np.isnan(row['Number of incomplete ablations.1']):
+            nr_incompletely_ablated = row['Number of incomplete ablations.1']
 
     if nr_completely_ablated == no_of_lesions:
         for i in range(0, int(nr_completely_ablated)):
@@ -45,14 +45,14 @@ for idx, patient_id in enumerate(patient_ids):
     elif nr_incompletely_ablated == no_of_lesions:
         for i in range(0,  int(nr_incompletely_ablated)):
             LTP.append(False)
-    else:
+    elif nr_completely_ablated != no_of_lesions and nr_incompletely_ablated != no_of_lesions:
         print('this patient must be updted for LTP:', patient_id)
         for i in range(0, int(no_of_lesions)):
             LTP.append(None)
 
-print(np.array(no_lesions_total).sum())
+print('No of lesions in RedCap:', np.array(no_lesions_total).sum())
 df_radiomics['LTP'] = LTP
 
-writer = pd.ExcelWriter('Radiomics_Radii_Chemo_LTP_MAVERRIC.xlsx')
+writer = pd.ExcelWriter('Radiomics_Radii_Chemo_LTP_MAVERRIC_141019.xlsx')
 df_radiomics.to_excel(writer, sheet_name='radiomics', index=False, float_format='%.4f')
 writer.save()
