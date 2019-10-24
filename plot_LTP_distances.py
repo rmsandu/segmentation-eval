9f54bbd0a51134eb18fac71d81b1cf07105816ff
raluca-san <ralucam.sandu@gmail.com>
2019-10-24 16:12:26 +0200

    #22 boxplots LTP surface distances

--- plot_LTP_distances.py
+++ plot_LTP_distances.py
@@ -3,13 +3,15 @@
 @author: Raluca Sandu
 """
 
-import pandas as pd
-import numpy as np
-import os
-import matplotlib.pyplot as plt
 import itertools
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
 import utils.graphing as gh
+
 plt.style.use('ggplot')
 
 
@@ -17,13 +19,11 @@ def flatten(a):
     return list(itertools.chain.from_iterable(a))
 
 
-#%%
+# %%
 
 
 input_file = r"C:\develop\segmentation-eval\Radiomics_Radii_Chemo_LTP_Distances_ECALSS.xlsx"
 flag_to_plot_subcapsular = False  # default value: false, only plot non-subcapsular lesions
-
-
 
 df_final = pd.read_excel(input_file)
 
@@ -37,8 +37,7 @@ df_final['vals'] = df_final['SurfaceDistances_Tumor2Ablation'].apply(lambda x: x
 df_final['vals'] = df_final['vals'].apply(lambda x: x.replace(']', ''))
 df_final['distances'] = df_final['vals'].apply(lambda x: x.split(','))
 
-
-#%%
+# %%
 surface_distances = []
 for idx, row in df_final.iterrows():
     dists = row['distances']
@@ -52,14 +51,13 @@ for idx, row in df_final.iterrows():
             continue
     surface_distances.append(list_dists)
 
-
 print(len(surface_distances))
 df_final['surface_distances'] = surface_distances
 
-#%% drop subcapsular lesions
+# %% drop subcapsular lesions
 df_final = df_final[df_final['Proximity_to_surface'] == flag_to_plot_subcapsular]
 print('No of lesions total:', str(len(df_final)))
-#%%
+# %%
 #
 # df1 = df_final.set_index('LTP')
 # df1.distances.explode().rename_axis('LTP').reset_index(name='sd')
@@ -68,22 +66,23 @@ res = df_final.groupby('LTP')['surface_distances'].apply(flatten).reset_index()
 
 # explode lists
 res = (res['surface_distances'].apply(pd.Series)
-              .stack()
-              .reset_index(level=1, drop=True)
-              .to_frame('distances')).reset_index()
-res.rename({'index':'LTP'}, axis=1, inplace=True)
+       .stack()
+       .reset_index(level=1, drop=True)
+       .to_frame('distances')).reset_index()
+res.rename({'index': 'LTP'}, axis=1, inplace=True)
 
 # plot the new data
-fig, ax = plt.subplots(figsize=(10,8))
-bp_dict = res.boxplot(column=['distances'],  notch=False, by='LTP', ax=ax, return_type='both', patch_artist=True,  showfliers=True)
+fig, ax = plt.subplots(figsize=(10, 8))
+bp_dict = res.boxplot(column=['distances'], notch=False, by='LTP', ax=ax, return_type='both', patch_artist=True,
+                      showfliers=True)
 
-for row_key, (ax,row) in bp_dict.iteritems():
+for row_key, (ax, row) in bp_dict.iteritems():
     ax.set_xlabel('')
-    for i,box in enumerate(row['fliers']):
+    for i, box in enumerate(row['fliers']):
         box.set_marker('o')
         # box.set_edgecolor('RoyalBlue')
-    for i,box in enumerate(row['boxes']):
-        if i==0:
+    for i, box in enumerate(row['boxes']):
+        if i == 0:
             box.set_facecolor('ForestGreen')
             box.set_edgecolor('DarkGreen')
         else:
@@ -97,7 +96,7 @@ for row_key, (ax,row) in bp_dict.iteritems():
 
 xticklabels = ['No LTP', 'LTP']
 xtickNames = plt.setp(ax, xticklabels=xticklabels)
-plt.setp(xtickNames,  fontsize=12, color='black')
+plt.setp(xtickNames, fontsize=12, color='black')
 plt.ylabel('Tumor to Ablation Euclidean Surface Distances [mm]', fontsize=12, color='black')
 ax.tick_params(colors='black')
 # [ax_tmp.set_xlabel('aaaa') for ax_tmp in np.asarray(bp).reshape(-1)]
@@ -107,13 +106,13 @@ fig.suptitle('')
 # ax.set_ylim([-10, 17])
 figpathHist = os.path.join("figures", "boxplot LTP ablation margin_not_subcapsular_outliers")
 gh.save(figpathHist, ext=['png'], close=True)
-#%%
-fig, ax = plt.subplots(figsize=(10,8))
+# %%
+fig, ax = plt.subplots(figsize=(10, 8))
 bp_dict = df_final.boxplot(column='Ablation Volume [ml]', by='LTP', ax=ax, return_type='both', patch_artist=True)
 
-for row_key, (ax,row) in bp_dict.iteritems():
+for row_key, (ax, row) in bp_dict.iteritems():
     ax.set_xlabel('')
-    for i,box in enumerate(row['boxes']):
+    for i, box in enumerate(row['boxes']):
         box.set_facecolor('CornflowerBlue')
         box.set_edgecolor('RoyalBlue')
     for i, box in enumerate(row['medians']):
@@ -124,7 +123,7 @@ for row_key, (ax,row) in bp_dict.iteritems():
 
 xticklabels = ['No LTP', 'LTP']
 xtickNames = plt.setp(ax, xticklabels=xticklabels)
-plt.setp(xtickNames,  fontsize=12, color='black')
+plt.setp(xtickNames, fontsize=12, color='black')
 plt.ylabel('Ablation Volume [ml]', fontsize=12, color='black')
 ax.tick_params(colors='black')
 # [ax_tmp.set_xlabel('aaaa') for ax_tmp in np.asarray(bp).reshape(-1)]
@@ -134,15 +133,15 @@ fig.suptitle('')
 ax.set_ylim([-1, 100])
 figpathHist = os.path.join("figures", "boxplot LTP ablation volumes_not_subcapsular")
 gh.save(figpathHist, ext=['png'], close=True)
-#%%
-fig, ax = plt.subplots(figsize=(10,8))
+# %%
+fig, ax = plt.subplots(figsize=(10, 8))
 # props = dict(boxes="DarkGreen", whiskers="DarkOrange", medians="DarkBlue", caps="Gray")
 bp_dict = df_final.boxplot(column='Tumour Volume [ml]', by='LTP', ax=ax, return_type='both',
-                         patch_artist=True, labels=['LTP', 'No LTP'])
+                           patch_artist=True, labels=['LTP', 'No LTP'])
 
-for row_key, (ax,row) in bp_dict.iteritems():
+for row_key, (ax, row) in bp_dict.iteritems():
     ax.set_xlabel('')
-    for i,box in enumerate(row['boxes']):
+    for i, box in enumerate(row['boxes']):
         box.set_facecolor('DarkOrange')
         box.set_edgecolor('Firebrick')
     for i, box in enumerate(row['medians']):
@@ -153,7 +152,7 @@ for row_key, (ax,row) in bp_dict.iteritems():
 
 xticklabels = ['No LTP', 'LTP']
 xtickNames = plt.setp(ax, xticklabels=xticklabels)
-plt.setp(xtickNames,  fontsize=12, color='black')
+plt.setp(xtickNames, fontsize=12, color='black')
 plt.ylabel('Tumor Volume [ml]', fontsize=12, color='black')
 ax.tick_params(colors='black')
 # [ax_tmp.set_xlabel('aaaa') for ax_tmp in np.asarray(bp).reshape(-1)]
