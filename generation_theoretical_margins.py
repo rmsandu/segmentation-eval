1a3ba412f57a204f227f21c65916c111d3a4cb3c
raluca-san <ralucam.sandu@gmail.com>
2019-12-09 15:41:58 +0100

    closes #43 convert volume to mm cubes

--- generation_theoretical_margins.py
+++ generation_theoretical_margins.py
@@ -2,22 +2,19 @@
 """
 @author: Raluca Sandu
 """
+import os
 from math import pi
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from scipy.interpolate import griddata
+from sklearn import linear_model
+
+import utils.graphing as gh
 
 
-# read the ablation params from the brochure.
-# interpolate if value not available. Nan if outside the range.
-# read the tumor axis and average them out.
-# add 10 and 5 mm to the radius
-# plug in into the sphere formula
-#plot against the predictive volume
-
-
-def interpolation_fct(df_ablation, df_radiomics, title, fontsize=24, flag_tumor=None):
+def interpolation_fct(df_ablation, df_radiomics, title, fontsize=24, flag_tumor=None, lin_regr=False):
     """
 
     :param df_ablation:
@@ -46,7 +43,7 @@ def interpolation_fct(df_ablation, df_radiomics, title, fontsize=24, flag_tumor=
                                                   power_and_time_effective, method='linear')
     ablation_vol_interpolated_brochure = ablation_vol_interpolated_brochure.reshape(len(df_radiomics), )
     ablation_vol_measured = np.asarray(df_radiomics['Ablation Volume [ml]']).reshape(len(df_radiomics), )
-    #%% PLOT Tumor Volume vs PAV (per colours subcapsular vs. non-subcapsular)
+    # %% PLOT Tumor Volume vs PAV (per colours subcapsular vs. non-subcapsular)
     # groupby needed.
     fig, ax = plt.subplots()
     if flag_tumor == 'Tumour Volume [ml]':
@@ -54,19 +51,76 @@ def interpolation_fct(df_ablation, df_radiomics, title, fontsize=24, flag_tumor=
     elif flag_tumor == 'Tumour Volume + 10mm margin [ml]':
         tumor_volume = df_radiomics['Tumour Volume + 10mm margin [ml]']
     subcapsular = df_radiomics['Proximity_to_surface']
-    df = pd.DataFrame(data=dict(x=ablation_vol_interpolated_brochure, y=tumor_volume, subcapsular=subcapsular))
+    df = pd.DataFrame(data=dict(x=tumor_volume, y=ablation_vol_interpolated_brochure, subcapsular=subcapsular))
     df.dropna(inplace=True)
     grouped = df.groupby(subcapsular)
+    labels = ['Subcapsular Tumors', 'Deep Tumors']
+    for i, (name, group) in enumerate(grouped):
+        plt.scatter(group.x, group.y, alpha=0.5, label=labels[i], s=100)
+    plt.legend(title=title + '(n = ' + str(len(df)) + ' )', title_fontsize=fontsize, fontsize=fontsize)
+    plt.ylabel('Predicted Ablation Volume [ml]', fontsize=fontsize)
+    plt.xlabel(flag_tumor, fontsize=fontsize)
+    plt.xlim([0, 100])
+    plt.ylim([0, 100])
+    ax.tick_params(axis='y', labelsize=fontsize, color='k')
+    ax.tick_params(axis='x', labelsize=fontsize, color='k')
+    plt.tick_params(labelsize=fontsize, color='black')
+    if lin_regr is True:
+        X = np.asarray(df.x).reshape(len(df), 1)
+        Y = np.asarray(df.y).reshape(len(df), 1)
+        regr = linear_model.LinearRegression()
+        regr.fit(X, Y)
+        SS_tot = np.sum((Y - np.mean(Y)) ** 2)
+        residuals = Y - regr.predict(X)
+        SS_res = np.sum(residuals ** 2)
+        r_squared = 1 - (SS_res / SS_tot)
+        correlation_coef = np.corrcoef(X[:, 0], Y[:, 0])[0, 1]
+        label_r2 = r'$R^2:{0:.2f}$'.format(r_squared)
+        label_r = r'$r: {0:.2f}$'.format(correlation_coef)
+        ax.tick_params(axis='y', labelsize=fontsize, color='k')
+        ax.tick_params(axis='x', labelsize=fontsize, color='k')
+        plt.tick_params(labelsize=fontsize, color='black')
+        plt.plot([], [], ' ', label=label_r)
+        plt.plot([], [], ' ', label=label_r2)
+        reg = plt.plot(X, regr.predict(X), color='black', linewidth=1.5, label='Linear Regression')
+        plt.legend(title=title, title_fontsize=fontsize, fontsize=fontsize)
+    plt.show()
+    figpath = os.path.join("figures", title +  '_PAV_vs_' + flag_tumor)
+    gh.save(figpath, width=12, height=12, ext=["png"], close=True, tight=True, dpi=600)
 
 
 if __name__ == '__main__':
-    df_brochure = pd.read_excel(r"C:\develop\segmentation-eval\Ellipsoid_Brochure_Info.xlsx")
+    df_ablation = pd.read_excel(r"C:\develop\segmentation-eval\Ellipsoid_Brochure_Info.xlsx")
     df_radiomics = pd.read_excel(r"C:\develop\segmentation-eval\Radiomics_MAVERRIC_ablation_curated.xlsx")
-    # TODO: extract Tumor Volume using getMeshVolumeFeatureValue() from pyRadiomics
-    # radius_tumor = (df_radiomics['least_axis_length_tumor'] +
-    #                 df_radiomics['minor_axis_length_tumor'] + df_radiomics['major_axis_length_tumor']) / 3
-    # we have volume of sphere in voxels...we can try to find out the radius from there, rather than averaging out the 3 radii of an ellipsoid
-    radius = ((3 * df_radiomics['Tumour Volume [ml]'])/4*pi) ** (1. / 3)
+    radius = ((3 * df_radiomics['Tumour Volume [ml]'] * 1000) / 4 * pi) ** (1. / 3)
     df_radiomics['Tumor_Radius'] = radius
     df_radiomics['Tumour Volume + 10mm margin [ml]'] = (4 * pi * (df_radiomics['Tumor_Radius'] + 10) ** 3) / 3000
     df_radiomics['Tumor Volume + 5mm margin [ml]'] = (4 * pi * (df_radiomics['Tumor_Radius'] + 5) ** 3) / 3000
+
+    df_amica = df_ablation[df_ablation['Device_name'] == 'Amica (Probe)']
+    df_amica.reset_index(inplace=True)
+    df_angyodinamics = df_ablation[df_ablation['Device_name'] == 'Angyodinamics (Acculis)']
+    df_angyodinamics.reset_index(inplace=True)
+    df_covidien = df_ablation[df_ablation['Device_name'] == 'Covidien (Covidien MWA)']
+    df_covidien.reset_index(inplace=True)
+    # df_radiomics = df_radiomics[(df_radiomics['Energy [kj]'] > 0) & (df_radiomics['Energy [kj]'] <= 100)]
+    df_radiomics_amica = df_radiomics[df_radiomics['Device_name'] == 'Amica (Probe)']
+    df_radiomics_amica.reset_index(inplace=True)
+    df_radiomics_angyodinamics = df_radiomics[df_radiomics['Device_name'] == 'Angyodinamics (Acculis)']
+    df_radiomics_angyodinamics.reset_index(inplace=True)
+    df_radiomics_covidien = df_radiomics[df_radiomics['Device_name'] == 'Covidien (Covidien MWA)']
+    df_radiomics_covidien.reset_index(inplace=True)
+
+    interpolation_fct(df_angyodinamics, df_radiomics_angyodinamics, 'Solero', fontsize=24,
+                      flag_tumor='Tumour Volume + 10mm margin [ml]')
+    interpolation_fct(df_angyodinamics, df_radiomics_angyodinamics, 'Solero', fontsize=24,
+                      flag_tumor='Tumour Volume [ml]')
+    interpolation_fct(df_amica, df_radiomics_amica, 'Amica', fontsize=24,
+                      flag_tumor='Tumour Volume + 10mm margin [ml]')
+    interpolation_fct(df_covidien, df_radiomics_covidien, 'Covidien', fontsize=24,
+                      flag_tumor='Tumour Volume + 10mm margin [ml]')
+    interpolation_fct(df_amica, df_radiomics_amica, 'Amica', fontsize=24,
+                      flag_tumor='Tumour Volume [ml]')
+    interpolation_fct(df_covidien, df_radiomics_covidien, 'Covidien', fontsize=24,
+                      flag_tumor='Tumour Volume [ml]')
+
