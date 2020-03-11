bc80fb3e84a0528c663f66243311e2edce8fe4bb
raluca-san <ralucam.sandu@gmail.com>
2020-03-11 15:28:06 +0100

    add code for inner ellipsoid

--- scripts/ellipsoid_inner_outer.py
+++ scripts/ellipsoid_inner_outer.py
@@ -17,7 +17,7 @@ import DicomReader
 import utils.graphing as gh
 
 
-def mvee(points, tol=0.001, flag='outer'):
+def mvee(points, tol=0.001, flag_outer_ellipsoid=True):
     """
     Find the minimum volume ellipse.
     Return A, c where the equation for the ellipse given in "center form" is
@@ -26,40 +26,28 @@ def mvee(points, tol=0.001, flag='outer'):
     points = np.asmatrix(points)
     N, d = points.shape
     Q = np.column_stack((points, np.ones(N))).T
-    err = 1.0
     u = np.ones(N) / N
-    #  inner ellipse: if d < 1+tol_dist
-    #  outer ellipse : while err > tol:
-    if flag == 'inner':
-        while err < 1 + tol:
-            # assert u.sum() == 1 # invariant
-            X = Q * np.diag(u) * Q.T
-            M = np.diag(Q.T * la.inv(X) * Q)
-            jdx = np.argmax(M)
-            step_size = (M[jdx] - d - 1.0) / ((d + 1) * (M[jdx] - 1.0))
-            new_u = (1 - step_size) * u
-            new_u[jdx] += step_size
-            err = la.norm(new_u - u)
-            u = new_u
-    elif flag == 'outer':
-        err = 1.0 + tol
-        while err > tol:
-            X = Q * np.diag(u) * Q.T
-            M = np.diag(Q.T * la.inv(X) * Q)
-            jdx = np.argmax(M)
-            step_size = (M[jdx] - d - 1.0) / ((d + 1) * (M[jdx] - 1.0))
-            new_u = (1 - step_size) * u
-            new_u[jdx] += step_size
-            err = la.norm(new_u - u)
-            u = new_u
+    err = 1.0 + tol
+    while err > tol:
+        X = Q * np.diag(u) * Q.T
+        M = np.diag(Q.T * la.inv(X) * Q)
+        jdx = np.argmax(M)
+        step_size = (M[jdx] - d - 1.0) / ((d + 1) * (M[jdx] - 1.0))
+        new_u = (1 - step_size) * u
+        new_u[jdx] += step_size
+        err = la.norm(new_u - u)
+        u = new_u
 
-    c = u * points
+    c = u * points  # center of ellipsoid
     A = la.inv(points.T * np.diag(u) * points - c.T * c) / d
-    return np.asarray(A), np.squeeze(np.asarray(c))
-    # U, D, V = la.svd(np.asarray(A))
-    # rx, ry, rz = 1. / np.sqrt(D)
-    # # # return the radii
-    # return rx, ry, rz
+    # return np.asarray(A), np.squeeze(np.asarray(c))
+    if flag_outer_ellipsoid is True:
+        U, D, V = la.svd(np.asarray(A))
+    else:
+        U, D, V = la.svd(3*np.asarray(A))
+    rx, ry, rz = 1. / np.sqrt(D)
+    # # return the radii
+    return rx, ry, rz
 
 
 def volume_ellipsoid(a, b, c):
@@ -120,39 +108,14 @@ def get_surface_points(img_file):
     return surface_points, spacing
 
 
-def volume_outer_ellipsoid(img):
-    """
-
-    :param img: DICOM SimpleITK image
-    :return: volume in ml
-    """
-    surface_points, spacing = get_surface_points(img)
-    try:
-        rx, ry, rz = mvee(surface_points, flag='outer')
-    except Exception:
-        print('The points cannot be approximated with an ellipsoid...returning NaN for volume')
-        return np.nan
-    vol_formula_outer = volume_ellipsoid(rx, ry, rz)
-    return vol_formula_outer
-
-def volume_inner_ellipsoid(img):
-    """
-
-    :param img: DICOM SimpleITK image
-    :return: volume in ml
-    """
-    # surface_points, spacing = get_surface_points(img)
-    # try:
-    #     rx, ry, rz = mvee(surface_points, flag='inner')
-    #     print('The points cannot be approximated with an ellipsoid...returning NaN for volume')
-    #     return np.nan
-    # vol_formula_inner = volume_ellipsoid(rx, ry, rz)
-    # TODO 5.03.2020 - replace inner volume formula
-    vol_formula_inner = np.nan
-    return vol_formula_inner
-
-
 def plot_ellipsoid(A, centroid, color):
+    """
+
+    :param A:
+    :param centroid:
+    :param color:
+    :return:
+    """
     U, D, V = la.svd(A)
     rx, ry, rz = 1. / np.sqrt(D)
     print(rx, ry, rz)
@@ -170,7 +133,7 @@ def plot_ellipsoid(A, centroid, color):
 
 
 if __name__ == '__main__':
-    dir_name = r"C:\tmp_patients\Pat_MAV_BE_B02_\Study_0\Series_7\CAS-One Recordings\2019-09-05_11-52-19\Segmentations\SeriesNo_28\SegmentationNo_0"
+    dir_name = r""
     points, spacing = get_surface_points(dir_name)
     # points = np.array([[0.53135758, -0.25818091, -0.32382715],
     #                    [0.58368177, -0.3286576, -0.23854156, ],
@@ -182,7 +145,7 @@ if __name__ == '__main__':
     #                    [0.62311759, -0.0101013, -0.1863324],
     #                    [0.62311759, -0.2101013, -0.1863324]])
     spacing = [1, 1, 1]
-    A_outer, centroid_outer = mvee(points, flag='outer')
+    A_outer, centroid_outer = mvee(points)
     U, D, V = la.svd(np.asarray(A_outer))
     rx, ry, rz = 1. / np.sqrt(D)
     rx_inner, ry_inner, rz_inner = rx / 3, ry / 3, rz / 3
@@ -190,21 +153,16 @@ if __name__ == '__main__':
     vol_spacing_outer = volume_ellipsoid_spacing(rx, ry, rz, spacing)
     print('volume formula:', vol_formula_outer)
     print('volume spacing ellipsoid scikit-image:', vol_spacing_outer)
-
+#%% PLOT
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.scatter(points[:, 0], points[:, 1], points[:, 2], c='orange', label='Ablation Segmentation')
     plot_ellipsoid(A_outer, centroid_outer, 'blue')
+    plot_ellipsoid(2*A_outer, centroid_outer, 'green')
     plt.legend(loc='best')
     plt.show()
     timestr = time.strftime("%H%M%S-%Y%m%d")
     file_dir = r"C:\develop\segmentation-eval\figures"
     filepath = os.path.join(file_dir, 'ellipsoid_' + timestr)
     gh.save(filepath, width=12, height=12, tight=True)
-    # plot_ellipsoid(A_inner, centroid_inner, 'red')
 
-    # rx, ry, rz = mvee(points, flag='inner')
-    # vol_formula_inner = volume_ellipsoid(rx, ry, rz)
-    # vol_spacing_inner = volume_ellipsoid_spacing(rx, ry, rz, spacing)
-    # print('volume formula:', vol_formula_inner)
-    # print('volume spacing ellipsoid scikit-image:', vol_spacing_inner)
