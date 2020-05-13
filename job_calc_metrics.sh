a938ad7aecc02c3dbe07ca61366cdca5139f46ca
Iwan Paolucci <iwan.paolucci@artorg.unibe.ch>
2020-05-13 23:34:41 +0200

    added ubelix job

---
+++ job_calc_metrics.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# You must specify a valid email address!
+#SBATCH --mail-user=iwan.paolucci@artorg.unibe.ch
+
+# Mail on NONE, BEGIN, END, FAIL, REQUEUE, ALL
+#SBATCH --mail-type=none
+
+# Job name
+#SBATCH --job-name="Calculate metrics"
+
+# Runtime and memory
+#SBATCH --time=02:00:00
+#SBATCH --mem-per-cpu=4G
+
+# Partition
+#SBATCH --partition=all
+
+#### Your shell commands below this line ####
+module load Python/3.6.4-foss-2018a
+
+source ~/envs/maverric/bin/activate
+
+cd ~/src/segmentation-eval
+
+python A_read_files_info.py  -o output -b /storage/research/artorg_igt/Projects/MAVERRIC/batch.xlsx
+
